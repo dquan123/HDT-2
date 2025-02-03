530ae5f3536f772fc344b0f551a7d20fa04e4b58
MiguelRosas11 <roguemijose@gmail.com>
2025-02-03 11:42:42 -0600

    Prueba 1

--- ht2/src/main/java/edu/uvg/App.java
+++ ht2/src/main/java/edu/uvg/App.java
@@ -6,11 +6,16 @@ package edu.uvg;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-
-        System.out.println( "Hello World!" );
-        System.out.println("Hola mund");
-
+    public int suma(int n1, int n2){
+        return n1 + n2;
+    }
+    public int resta(int n1, int n2){
+        return n1 - n2;
+    }
+    public int division(int n1, int n2){
+        return n1/n2;
+    }
+    public int multiplicacion(int n1, int n2){
+        return n1*n2;
     }
 }
