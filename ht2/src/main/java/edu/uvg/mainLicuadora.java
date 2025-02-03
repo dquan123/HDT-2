fc0ed613be7d897162ba1c316ff682fec8d085e6
Diego Quan <dquan@delicatessen502.com>
2025-02-03 12:48:07 -0600

    Correcciones main

--- ht2/src/main/java/edu/uvg/mainLicuadora.java
+++ ht2/src/main/java/edu/uvg/mainLicuadora.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 public class mainLicuadora {
     public static void main(String[] args) {
-        String ruta = "\"C:\\Users\\rogue\\OneDrive\\Escritorio\\hola mundo.txt\"";  //Ruta del archivo txt
+        String ruta = "C:\\Users\\dquan\\OneDrive\\Documentos\\Diego Quan\\UVG\\Ciclo 3\\Algoritmos y Estructura de datos\\Hoja de trabajo 2\\HDT-2\\ht2\\src\\main\\java\\edu\\uvg\\Postfix.txt";
         try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
             String linea = br.readLine();
             System.out.println(linea);
