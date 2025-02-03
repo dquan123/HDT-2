e869c92fe7626a02a3a1c3a066bf0b28ab64c6cd
MiguelRosas11 <roguemijose@gmail.com>
2025-02-03 12:37:33 -0600

    ruta prueba

--- ht2/src/main/java/edu/uvg/mainLicuadora.java
+++ ht2/src/main/java/edu/uvg/mainLicuadora.java
@@ -5,9 +5,10 @@ import java.io.IOException;
 
 public class mainLicuadora {
     public static void main(String[] args) {
-        String ruta = "";  //Ruta del archivo txt
+        String ruta = "\"C:\\Users\\rogue\\OneDrive\\Escritorio\\hola mundo.txt\"";  //Ruta del archivo txt
         try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
             String linea = br.readLine();
+            System.out.println(linea);
         }catch (IOException e) {
             System.out.println("Error al leer el archivo: " + e.getMessage());
         }
