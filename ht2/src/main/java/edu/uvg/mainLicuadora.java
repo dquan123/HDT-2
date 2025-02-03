62e55a73cb07e95ade2dcba9a3f889eb81eca1f9
MiguelRosas11 <roguemijose@gmail.com>
2025-02-03 12:56:26 -0600

    comentarios main

--- ht2/src/main/java/edu/uvg/mainLicuadora.java
+++ ht2/src/main/java/edu/uvg/mainLicuadora.java
@@ -6,10 +6,9 @@ import java.io.IOException;
 public class mainLicuadora {
     public static void main(String[] args) {
         String ruta = "C:\\Users\\dquan\\OneDrive\\Documentos\\Diego Quan\\UVG\\Ciclo 3\\Algoritmos y Estructura de datos\\Hoja de trabajo 2\\HDT-2\\ht2\\src\\main\\java\\edu\\uvg\\Postfix.txt";
-        try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
-            String linea = br.readLine();
-            System.out.println(linea);
-        }catch (IOException e) {
+        try (BufferedReader br = new BufferedReader(new FileReader(ruta))) { //intenta leer el archivo .txt
+            String linea = br.readLine(); //lee una linea y la guarda en el string linea
+        }catch (IOException e) {//Si no logra leer el archivo arroja una excepción
             System.out.println("Error al leer el archivo: " + e.getMessage());
         }
     }
