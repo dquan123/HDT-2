ee4f0a4283d6e461578cdd1045b955c39ff856ab
MiguelRosas11 <roguemijose@gmail.com>
2025-02-03 12:07:33 -0600

    Leer archivo txt

--- ht2/src/main/java/edu/uvg/mainLicuadora.java
+++ ht2/src/main/java/edu/uvg/mainLicuadora.java
@@ -1,7 +1,16 @@
 package edu.uvg;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class mainLicuadora {
     public static void main(String[] args) {
-        
+        String ruta = "";  //Ruta del archivo txt
+        try (BufferedReader br = new BufferedReader(new FileReader(ruta))) {
+            String linea = br.readLine();
+        }catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
     }
 }
+
