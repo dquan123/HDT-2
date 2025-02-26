220b3781b60c98d858769ece8295202eb5b0d869
Diego Quan <dquan@delicatessen502.com>
2025-02-26 07:24:30 -0600

    Cambio de nombre

--- ht2/src/main/java/edu/uvg/main.java
+++ ht2/src/main/java/edu/uvg/main.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class mainLicuadora {
+public class main {
     public static void main(String[] args) {
         String ruta = "C:\\Users\\dquan\\OneDrive\\Documentos\\Diego Quan\\UVG\\Ciclo 3\\Algoritmos y Estructura de datos\\Hoja de trabajo 2\\HDT-2\\ht2\\src\\main\\java\\edu\\uvg\\Postfix.txt";
         
