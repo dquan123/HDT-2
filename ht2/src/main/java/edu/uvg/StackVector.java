d753be23cc74f9b8a03f767c4e72700a2dd78336
MiguelRosas11 <roguemijose@gmail.com>
2025-02-26 11:24:05 -0600

    Cuerpo de los métodos de la clase StackVector

--- ht2/src/main/java/edu/uvg/StackVector.java
+++ ht2/src/main/java/edu/uvg/StackVector.java
@@ -1,4 +1,34 @@
 package edu.uvg;
 
+import java.util.Vector;
+
 public class StackVector<T> extends AbstractStack<T>{
+    private Vector<T> stack;
+
+    public StackVector() {
+        stack = new Vector<>();
+    }
+
+    @Override
+    public void push(T item) {
+        stack.add(item);
+    }
+
+    @Override
+    public T pop() throws Exception {
+        if (isEmpty()) throw new Exception("Stack vacío");
+        return stack.remove(stack.size() - 1);
+    }
+
+    @Override
+    public T UltimoValor() throws Exception {
+        if (isEmpty()) throw new Exception("Stack vacío");
+        return stack.get(stack.size() - 1);
+    }
+
+    @Override
+    public int size() {
+        return stack.size();
+    }
 }
+
