a5a0aa2c799c594dc005135dd0b944f73d8e58aa
MiguelRosas11 <roguemijose@gmail.com>
2025-02-26 10:35:53 -0600

    Clase AbstractStack y comentario

--- ht2/src/main/java/edu/uvg/AbstractStack.java
+++ ht2/src/main/java/edu/uvg/AbstractStack.java
@@ -1,4 +1,8 @@
 package edu.uvg;
 
-public abstract class AbstractStack {
+public abstract class AbstractStack<T> implements iStack<T> {
+    @Override
+    public boolean isEmpty() { //Método que retorna el tamano igual a 0.
+        return size() == 0;
+    }
 }
