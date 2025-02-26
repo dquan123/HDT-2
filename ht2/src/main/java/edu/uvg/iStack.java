6a92f43f2ac9ad6a8a79ee04e966835b54a3dfc4
MiguelRosas11 <roguemijose@gmail.com>
2025-02-26 10:31:43 -0600

    Comentarios interfaz pila

--- ht2/src/main/java/edu/uvg/iStack.java
+++ ht2/src/main/java/edu/uvg/iStack.java
@@ -2,13 +2,13 @@ package edu.uvg;
 
 public interface iStack<T> {
 
-    void push(T item);
+    void push(T item); //Método para meter valores a la pila
 
-    T pop();
+    T pop() throws Exception; //Método para extraer valores de la pila
 
-    boolean isEmpty();
+    boolean isEmpty(); //Método para saber si la lista no está vacía
 
-    T peek();
+    T UltimoValor() throws Exception; //Método para conocer el último valor de la pila sin extraerlo
 
-    int size();
+    int size(); //Método para conocer cuántos items hay en la pila
 }
