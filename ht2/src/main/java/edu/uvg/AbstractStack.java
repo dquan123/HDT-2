2bc12ca8ee329ddeb18b1fe8cd1940af20d12c0a
MiguelRosas11 <roguemijose@gmail.com>
2025-02-26 11:04:20 -0600

    Correción

--- ht2/src/main/java/edu/uvg/AbstractStack.java
+++ ht2/src/main/java/edu/uvg/AbstractStack.java
@@ -2,7 +2,7 @@ package edu.uvg;
 
 public abstract class AbstractStack<T> implements iStack<T> {
     @Override
-    public boolean isEmpty() { //Método que retorna el tamano igual a 0.
-        return size() == 0;
+    public boolean isEmpty() { //Método que retorna si el stack está vacío.
+        return size() == 0; //Si el tamano es de 0 retorna 0
     }
 }
