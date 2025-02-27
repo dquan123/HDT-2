8394de72599ab142dda9f69f4ba18fab556b4d80
Gonza <gon24170@uvg.edu.gt>
2025-02-27 11:11:33 -0600

    Comentarios en iCalculadora.

--- ht2/src/main/java/edu/uvg/iCalculadora.java
+++ ht2/src/main/java/edu/uvg/iCalculadora.java
@@ -1,4 +1,8 @@
 package edu.uvg;
 public interface iCalculadora {
+    
+    // Método para evaluar una expresión matemática y devolver el resultado como un entero
+    // Puede lanzar una excepción en caso de error en la evaluación
     int evaluate(String expresion) throws Exception;
 }
+
